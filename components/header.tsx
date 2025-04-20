c2676e7bc3135bf4688e8e4150c6875f565da052
Logan Van Wagoner <logangvw3@gmail.com>
2025-04-20 16:45:02 -0600

    centering the header so i don't go insane

--- components/header.tsx
+++ components/header.tsx
@@ -6,13 +6,13 @@ import { CommandMenu } from "./command-menu"
 export function Header() {
     return (
         <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex items-center justify-center">
-            <div className="container flex h-14 items-center justify-between">
+            <div className="container grid grid-cols-3 h-14">
                 <div></div>
-                <Link href="/" className="flex items-center gap-2 font-bold">
+                <Link href="/" className="flex items-center justify-center gap-2 font-bold">
                     <BookMarked className="h-5 w-5" />
                     <span>Family Plates</span>
                 </Link>
-                <div >
+                <div className="flex items-center justify-end mr-2 gap-2">
                     <ThemeToggle />
                     <CommandMenu />
                 </div>
